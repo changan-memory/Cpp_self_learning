76fd5629185952a39758ace8de0ac1efd4a30bf4
changan-memories <qinyuxiangzzu@outlook.com>
2025-02-10 22:19:06 +0800

    update

--- Cpp_7_classes_object_4/Cpp_7_classes_object_4/main.cpp
+++ Cpp_7_classes_object_4/Cpp_7_classes_object_4/main.cpp
@@ -4,43 +4,45 @@ using std::cin;
 using std::endl;
 
 
-class Stack {
-private:
-	int* _base = nullptr;	//C++11支持的成员变量缺省值
-	int _top = 0;
-	int _capacity = 0;
-public:
-	Stack(int defaultCapacity = 4) {
-		this->_base = (int*)malloc(sizeof(int) * defaultCapacity);
-		if (this->_base == nullptr) {
-			perror("malloc failed\n");
-			return;
-		}
-		this->_capacity = defaultCapacity;
-		this->_top = 0;
-	}
-	//注意：类中如果没有涉及资源申请时，拷贝构造函数是否写都可以；
-	// 一旦涉及到资源申请时，则拷贝构造函数是一定要写的，否则就是浅拷贝。
+//class Stack {
+//private:
+//	int* _base = nullptr;	//C++11支持的成员变量缺省值
+//	int _top = 0;
+//	int _capacity = 0;
+//public:
+//	Stack(int defaultCapacity = 4) {
+//		this->_base = (int*)malloc(sizeof(int) * defaultCapacity);
+//		if (this->_base == nullptr) {
+//			perror("malloc failed\n");
+//			return;
+//		}
+//		this->_capacity = defaultCapacity;
+//		this->_top = 0;
+//	}
+//	//注意：类中如果没有涉及资源申请时，拷贝构造函数是否写都可以；
+//	// 一旦涉及到资源申请时，则拷贝构造函数是一定要写的，否则就是浅拷贝。
+//
+//	//简单实现一个深拷贝  st2(st1)
+//	Stack(const Stack& stack) {
+//		this->_base = (int*)malloc(sizeof(int) * stack._capacity);
+//		if (this->_base == nullptr) {
+//			perror("malloc failed\n");
+//			return;
+//		}
+//		memcpy(this->_base, stack._base, sizeof(int) * stack._top);
+//		this->_top = stack._top;
+//		this->_capacity = stack._capacity;
+//	}
+//	~Stack() {
+//		cout << " ~Stack" << endl;
+//		free(this->_base);
+//		this->_base = nullptr;
+//		this->_capacity = 0;
+//		this->_top = 0;
+//	}
+//};
 
-	//简单实现一个深拷贝  st2(st1)
-	Stack(const Stack& stack) {
-		this->_base = (int*)malloc(sizeof(int) * stack._capacity);
-		if (this->_base == nullptr) {
-			perror("malloc failed\n");
-			return;
-		}
-		memcpy(this->_base, stack._base, sizeof(int) * stack._top);
-		this->_top = stack._top;
-		this->_capacity = stack._capacity;
-	}
-	~Stack() {
-		cout << " ~Stack" << endl;
-		free(this->_base);
-		this->_base = nullptr;
-		this->_capacity = 0;
-		this->_top = 0;
-	}
-};
+
 //拷贝构造  函数
 class Date {
 private:
@@ -72,7 +74,7 @@ private:
 	int _month;
 	int _day;
 public:
-	Date_1(int year = 2023, int month = 10, int day = 28) {
+	Date_1(int year = 2024, int month = 10, int day = 28) {
 		this->_year = year;
 		this->_month = month;
 		this->_day = day;
@@ -80,7 +82,7 @@ public:
 	//若未显式定义，编译器会生成默认的拷贝构造函数。 
 	// 默认的拷贝构造函数对象按内存存储按字节序完成拷贝，这种拷贝叫做浅拷贝，或者值拷贝。
 	//默认拷贝构造函数 做了什么？
-	//内置类型成员完成  值拷贝/浅拷贝     自定义类型，调用各自的构造函数，如果没有拷贝构造函数，则值拷贝
+	//内置类型成员完成  值拷贝/浅拷贝     自定义类型，调用各自的拷贝构造函数，如果没有拷贝构造函数，则值拷贝
 
 	/*Date_1(const Date_1& date) {
 		this->_year = date._year;
@@ -94,24 +96,64 @@ void Func(int i) {
 void Func(Date d) {
 	return;
 }
-int main() {
-	//日期类可以不写拷贝构造，默认生成的就可以
-	Date d1(2025, 2, 8);
-	Date d2(d1);
-
-	Func(d1);
-	Func(10);	//形参的拷贝，
-	
-	//值拷贝(浅拷贝)，有问题，两个指针存下了同一块空间的地址，析构时会对一块空间析构两次
-	// st1.push , 对 d1 对象的操作，也会影响 d2 对象
-	//会引发错误。  类似于  栈这样的对象，必须自己实现深拷贝
-	Stack st1;
-	Stack st2(st1);		//可以认为深拷贝 是为了  有资源的对象 的拷贝而设计的
-	return 0;
-}
+//int main() {
+//	//日期类可以不写拷贝构造，默认生成的就可以
+//	Date d1(2025, 2, 8);
+//	Date d2(d1);
+//
+//	Func(d1);
+//	Func(10);	//形参的拷贝，
+//	
+//	//值拷贝(浅拷贝)，有问题，两个指针存下了同一块空间的地址，析构时会对一块空间析构两次
+//	// st1.push , 对 d1 对象的操作，也会影响 d2 对象
+//	//会引发错误。  类似于  栈这样的对象，必须自己实现深拷贝
+//	Stack st1;
+//	Stack st2(st1);		//可以认为深拷贝 是为了  有资源的对象 的拷贝而设计的
+//	return 0;
+//}
 
 //建议加上 const
 		//const 是为了防止别人写错，防止别人写成以下代码
 		//date._year = _year;		//不仔细看真看不出来，赔了夫人又折兵，会出现随机值
 		//date._month = _month;
 		//date._day = _day;
+
+class Stack {
+private:
+	int* _base = nullptr;	//C++11支持的成员变量缺省值
+	int _top = 0;
+	int _capacity = 0;
+public:
+	Stack(int defaultCapacity = 4) {
+		this->_base = (int*)malloc(sizeof(int) * defaultCapacity);
+		if (this->_base == nullptr) {
+			perror("malloc failed\n");
+			return;
+		}
+		this->_capacity = defaultCapacity;
+		this->_top = 0;
+	}
+	//简单实现一个深拷贝  st2(st1)
+	Stack(const Stack& stack) {
+		this->_base = (int*)malloc(sizeof(int) * stack._capacity);
+		if (this->_base == nullptr) {
+			perror("malloc failed\n");
+			return;
+		}
+		memcpy(this->_base, stack._base, sizeof(int) * stack._top);
+		this->_top = stack._top;
+		this->_capacity = stack._capacity;
+	}
+	~Stack() {
+		cout << " ~Stack" << endl;
+		free(this->_base);
+		this->_base = nullptr;
+		this->_capacity = 0;
+		this->_top = 0;
+	}
+};
+int main() {
+	Stack st1;
+	Stack st2(st1);
+	return 0;
+}
