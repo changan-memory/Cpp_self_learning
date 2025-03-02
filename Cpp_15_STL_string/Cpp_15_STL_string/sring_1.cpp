f5d331d896c92459b9c1be435aeee4b1c7934b8d
changan-memories <qinyuxiangzzu@outlook.com>
2025-03-02 17:49:12 +0800

    update string iterator

---
+++ Cpp_15_STL_string/Cpp_15_STL_string/sring_1.cpp
@@ -0,0 +1,80 @@
+//#include <iostream>
+//#include <string>
+//using namespace std;
+//
+////C strxxx 系列库函数
+////Cpp string 类 
+//// basic use
+//void Test_use_construct() {
+//	/*string s1;
+//	std::string s2(s1);
+//	std::string name("yuxiang qin");
+//	name = "qin yuxiang";
+//	cout << name << endl;*/
+//
+//	// string 的构造
+//	string s1;	//默认构造  string();
+//	string s2("zhangsan");	// string (const char* s)
+//	string s3("hello world");
+//	string s4(10, 'p');		// string (const char* s, size_t n)
+//	string s5(s2);	//string (const string& str);
+//
+//	cout << s1 << endl;
+//	cout << s2 << endl;
+//	cout << s3 << endl;
+//	cout << s4 << endl;
+//	cout << s5 << endl;
+//
+//	string s6(s3, 6, 5);	// string (const string& str, size_t pos, size_t len = npos)
+//	string s7(s3, 6);	//缺省参数值是 -1 size_t = -1 ,是整形的最大值
+//	cout << s6 << " " << s7 << endl;
+//
+//	string url("https://cplusplus.com/reference/string/string/string/");
+//	string sub1(url, 0, 5);
+//	string sub2(url, 8, 13);
+//	string sub3(url, 22);
+//	cout << sub1 << endl;
+//	cout << sub2 << endl;
+//	cout << sub3 << endl;
+//
+//	// string 的=重载
+//	s3 = s1;	//string& operator= (const string& str);
+//	s2 = "I LOVE YOU";		//string& operator= (const char* s);
+//	s4 = "X";				//string& operator= (char c);
+//}
+////class string {
+////operator+=(char ch);
+////operator+=(const char* str);
+////private:
+////	char* _str;
+////	size_t _size;
+////	size_t capacity;
+////};
+//void Test_use_2() {
+//	//增
+//	string s1("hello world");
+//	//尾插一个字符
+//	s1.push_back('  ');
+//	//尾插一个字符串
+//	s1.append("world");
+//
+//	s1 += ' ';
+//	s1 += "world";
+//	cout << s1 << endl;
+//
+//	// 要求把x转换为string对象
+//	size_t x = 0;
+//	cin >> x;
+//	string xstr;
+//	while (x) {
+//		size_t val = x % 10;
+//		xstr += ('0' + val);
+//		x /= 10;
+//	}
+//	cout << xstr << endl;
+//}
+//int main() {
+//	//Test_use_construct();
+//	Test_use_2();
+//	return 0;
+//}
---
+++ Cpp_15_STL_string/Cpp_15_STL_string/string_2.cpp
@@ -0,0 +1,99 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <list>
+#include <algorithm>
+using std::cout;
+using std::endl;
+using std::string;
+using std::vector;
+using std::list;
+using namespace std;
+//test operator[]
+void Test_1() {
+	string s0;
+	string s1("hello world");	//底层是一个字符数组，结尾有一个\0
+	cout << s1 << endl;
+	cout << s1.size() << endl;	// \0不算大小，size是 11,\0是一个标识字符串结束的特殊字符，不算有效字符
+
+	for (size_t i = 0; i < s1.size(); ++i) {
+		cout << s1[i] << " ";
+	}
+	cout << endl;
+	for (size_t i = 0; i < s1.size(); ++i) {
+		s1[i] += 1;
+		cout << s1[i] << " ";
+	}
+
+	char s3[] = "hello world";
+	s3[0]++;	//底层: *(s3+0)
+	s1[0]++;	//底层: s1.operator[](&s1, 0)
+}
+//迭代器，暂且理解成像指针一样的东西
+void Test_2() {
+	string s1("hello world");	//底层是一个字符数组，结尾有一个\0
+	string::iterator str_it = s1.begin();
+	while (str_it != s1.end()) {
+		cout << *str_it;
+		++str_it;
+	}
+	cout << endl;
+	
+	//迭代器的底层可能是指针，也可能是别的实现
+	//范围for配合auto
+	//范围for 底层会被替换为迭代器，只要支持迭代器的容器，就支持范围for
+	//for (char& ch : s1) {
+	for (auto& ch : s1) {
+		cout << ch;
+	}
+	cout << endl;
+
+	//连续的空间才能用[下标]来访问
+	//迭代器才是容器主流的访问方式
+	vector<int> vec;
+	vec.push_back(2);
+	vec.push_back(4);
+	vec.push_back(6);
+	vector<int>::iterator vit = vec.begin();
+	while (vit != vec.end()) {
+		cout << *vit << " ";
+		vit++;
+	}
+	cout << endl;
+	//任何容器都支持迭代器，并且访问方式类似
+	list<int> ls;
+	ls.push_back(20);
+	ls.push_back(40);
+	ls.push_back(60);
+	list<int>::iterator lsit = ls.begin();
+	while (lsit != ls.end()) {
+		cout << *lsit << " ";
+		lsit++;
+	}
+	cout << endl;
+	//[]只能用于访问数组结构的容器，但迭代器可以访问任何容器,(树,哈希表等)
+	//迭代器跟容器和算法进行配合
+	std::reverse(vec.begin(), vec.end());
+	std::reverse(ls.begin(), ls.end());
+	for (auto& e : vec) {
+		cout << e << " ";
+	}
+	cout << endl;
+	for (auto& e : ls) {
+		cout << e << " ";
+	}
+	cout << endl;
+	//std::sort(ls.begin(), ls.end());	//不能对链表进行排序
+	std::sort(vec.begin(), vec.end());
+	for (auto& e : vec) {
+		cout << e << " ";
+	}
+	cout << endl;
+
+}
+
+int main() {
+	//Test_1();
+	Test_2();
+	return 0;
+}
