5a0362fda2a1c521d212328f88f27554d917e254
changan-memories <qinyuxiangzzu@outlook.com>
2025-03-12 22:05:00 +0800

    update

--- Cpp_15_STL_string/Cpp_15_STL_string/string_3_capacity.cpp
+++ Cpp_15_STL_string/Cpp_15_STL_string/string_3_capacity.cpp
@@ -11,10 +11,13 @@ using std::list;
 using namespace std;
 
 //capacity相关接口
-int main() {
+void Test_3_1() {
 	string s1("hello world");
 	cout << s1.size() << endl;
 	cout << s1.length() << endl;
 	cout << s1.capacity() << endl;
+}
+int main() {
+	Test_3_1();
 	return 0;
 }
