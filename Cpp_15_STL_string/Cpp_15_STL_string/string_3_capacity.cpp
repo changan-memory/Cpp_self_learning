438fffab57872a443a6be1c3d02434d1864780a7
changan-memories <qinyuxiangzzu@outlook.com>
2025-03-18 21:17:22 +0800

    update string

--- Cpp_15_STL_string/Cpp_15_STL_string/string_3_capacity.cpp
+++ Cpp_15_STL_string/Cpp_15_STL_string/string_3_capacity.cpp
@@ -11,16 +11,27 @@ using std::list;
 using namespace std;
 
 //capacity相关接口
+// string 的底层是一个类似于顺序表的结构
 void Test_3_1() {
 	string s1("hello world");
 	cout << s1.size() << endl;
 	cout << s1.length() << endl;
 	cout << s1.capacity() << endl;
 }
+void Test_3_2() {
+	string s;
+	s.reserve(100);
+	size_t sz = s.capacity();
+	for (int i = 0; i < 100; i++) {
+		s.push_back('c');
+		if (sz != s.capacity()) {
+			sz = s.capacity();
+			cout << " capacity changed" << endl;
+		}
+	}
+}
 int main() {
 	Test_3_1();
-
-
 
 
 	return 0;
--- Test_Cpp/Test_Cpp/main.cpp
+++ Test_Cpp/Test_Cpp/main.cpp
@@ -1,9 +1,50 @@
-class A {
+#include <iostream>
+#include <string>
+using namespace std;
 
+class Solution {
+public:
+	bool isLetter(const char ch) {
+		if ((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z')) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+	string reverseOnlyLetters(string s) {
+		if (s.empty())
+			return s;
+		int left = 0, right = s.size() - 1;
+
+		while (left < right) {
+			while (!isLetter(s[left])) {
+				if (left >= right) {
+					break;
+				}
+				++left;
+				
+			}
+			while (!isLetter(s[right])) {
+				if (left >= right) {
+					break;
+				}
+				--right;
+				
+			}
+			swap(s[left], s[right]);
+			++left;
+			--right;
+		}
+		return s;
+	}
 };
 
 int main() {
-	char* p = new char[100];
-	delete p;
+
+	string s1("7_28]");
+	Solution s;
+	string s_res = s.reverseOnlyLetters(s1);
+	cout << s_res << endl;
 	return 0;
 }
