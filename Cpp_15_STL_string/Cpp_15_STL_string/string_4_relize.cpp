b4a24d1b0974ea0ce5627e65482e9bac0f261029
changan-memories <qinyuxiangzzu@outlook.com>
2025-03-27 11:50:14 +0800

    update string relize

--- Cpp_15_STL_string/Cpp_15_STL_string/string_4_relize.cpp
+++ Cpp_15_STL_string/Cpp_15_STL_string/string_4_relize.cpp
@@ -7,6 +7,28 @@ void Test1() {
 
 	//无参构造
 	m_string::string s2;
+	const m_string::string s3("hello Linux");
+	/*for (int i = 0; i < s1.size(); ++i) {
+		s1[i]++;
+	}*/
+	for (int i = 0; i < s1.size(); ++i) {
+		cout << s1[i] << "";
+	}
+	cout << endl;
+	//迭代器
+	//m_string::string::const_iterator cit = s3.begin();
+	auto cit = s3.begin();
+	/*while (it != s3.end()) {
+		cout << *it << "";
+		++it;
+	}*/
+	//范围for底层 会被 替换成迭代器 
+	//C++11的语法支持范围for, 可以将自己的数据结构实现迭代器，就支持了范围for
+	for (auto& e : s3) {	//底层汇编和上面的while是一样的
+		cout << e << "";
+	}
+	cout << endl;
+	
 }
 int main() {
 
--- Cpp_15_STL_string/Cpp_15_STL_string/string_me.h
+++ Cpp_15_STL_string/Cpp_15_STL_string/string_me.h
@@ -1,10 +1,33 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 
 #include <iostream>
+#include <assert.h>
 using namespace std;
 
 namespace m_string {
 	class string {
+	public:
+		//STL中的迭代器，可能是指针，也可能不是指针
+		//string的迭代器本质上是char*
+		typedef char* iterator;
+		typedef const char* const_iterator;
+		
+		//普通对象的迭代器
+		iterator begin() {		
+			//return &_str[0];
+			return _str;	//数组名是第一个元素的地址
+		}
+		iterator end() {
+			//return &_str[_size];
+			return _str+_size;
+		}
+		//const对象迭代器
+		const_iterator begin() const {
+			return _str;	
+		}
+		const_iterator end() const {
+			return _str + _size;
+		}
 	private:
 		size_t _size;
 		size_t _capacity;
@@ -24,7 +47,8 @@ namespace m_string {
 		// 内置类型在 初始化列表/函数体内 初始化无所谓，自定义类型尽量在初始化列表
 		//string(const char* str = '\0')
 		//string(const char* str = nullptr)
-		//string(const char* str = "\0")
+		//string(const char* str = "\0")	//这个写法会让结尾有两个"\0"
+		//字符串的结束位置必须有\0
 		string(const char* str = "") {
 			_size = strlen(str);
 			_capacity = _size;	//capacity表示可以存放的下的字符个数
@@ -45,10 +69,54 @@ namespace m_string {
 			_str = nullptr;
 			_size = _capacity = 0;
 		}
-		//返回c_str
-		const char* c_str() {
+		//返回c_str			const 修饰this指针指向的对象，可以让普通对象和const对象都可以调
+		//函数内不修改对象，建议加上const
+		const char* c_str() const {
 			return _str;
 		}
-		
+		//返回_size
+		size_t size() const {
+			return _size;
+		}
+
+		char& operator[](size_t pos) {	//返回引用，读写版本
+			assert(pos < _size);
+			return _str[pos];
+		}	//const对象调const版本[]    普通对象调普通版本[]
+
+		const char& operator[](size_t pos) const {	//只读版本
+			assert(pos < _size);
+			return _str[pos];
+		}
+
+		//可以用reserve来实现扩容
+		void reserve(size_t spaceSize) {	//spaceSize指的是字符的个数
+			if (spaceSize > _capacity) {
+				char* newSpace = new char[spaceSize + 1];	//多开一个空间存放\0
+				strcpy(newSpace, _str);
+				delete[] _str;
+				_str = newSpace;
+				_capacity = spaceSize;
+			}
+		}
+		//+=的本质是push_back因此先实现push_back
+		void push_back(char ch) {
+			//先考虑扩容
+			if (_size == _capacity) {
+				//二倍扩容
+				reserve(_capacity == 0 ? 4 : _capacity * 2);
+			}
+			_str[_size++] = ch;	//放字符
+			_str[_size] = '\0';	//加上\0
+		}
+		void append(const char* str) {
+			size_t len = strlen(str);
+			if (_size + len > _capacity) {
+				//至少扩容到_size + len
+				reserve(_size + len);
+			}
+			strcpy(_str + _size, str);
+			_size += len;
+		}
 	};
 }
