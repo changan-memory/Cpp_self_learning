b2d0e8239e3bd6117fe044362921156dc4ab333f
changan-memories <qinyuxiangzzu@outlook.com>
2025-03-27 17:58:20 +0800

    update string relize

--- Cpp_15_STL_string/Cpp_15_STL_string/string_4_relize.cpp
+++ Cpp_15_STL_string/Cpp_15_STL_string/string_4_relize.cpp
@@ -28,10 +28,71 @@ void Test1() {
 		cout << e << "";
 	}
 	cout << endl;
+}
+void TestString_2() {
+	m_string::string s1("hello world");
+	cout << s1.c_str() << endl;
+	s1.push_back(' ');
+	s1.push_back('#');
+	s1.append(" hello Linux");
+	cout << s1.c_str() << endl;
+
+	m_string::string s2("hello world");
+	cout << s2.c_str() << endl;
+	s2 += ' ';
+	s2 += '#';
+	s2 += " hello Linux";
+	cout << s2.c_str() << endl;
+}
+void TestInsert() {
+	m_string::string s1("hello world");
+	cout << s1.c_str() << endl;
+	s1.insert(0, 3, '#');
+	cout << s1.c_str() << endl;
+
+	s1.insert(0, "123456");
+	cout << s1.c_str() << endl;
+}
+void TestErase() {
+	m_string::string s1("hello world");
+	cout << s1.c_str() << endl;
+	s1.erase(2, 2);
+	cout << s1.c_str() << endl;
 	
+	m_string::string s2("hello world");
+	s2.erase(3, 60);
+	cout << s2.c_str() << endl;
+
+	m_string::string s3("hello world");
+	s3.erase(2);
+	cout << s3.c_str() << endl;
+}
+void TestFind() {
+	m_string::string url = "https://cplusplus.com/reference/string/string/";
+	//分离 协议 域名  资源名
+	
+	size_t pos1 = url.find("://");
+	if (pos1 != m_string::string::npos) {
+		m_string::string protocol = url.substr(0, pos1);	//从下标0开始，查找指定长度的字符串
+		cout << protocol.substr(0, pos1).c_str() << endl;
+	}
+	m_string::string domain;
+	m_string::string uri;
+
+	size_t pos2 = url.find("/", pos1 + 3);
+	if (pos2 != string::npos) {
+		domain = url.substr(pos1 + 3, pos2 - (pos1 + 3));
+		uri = url.substr(pos2 + 1);
+	}
+	cout << url.c_str() << endl;
+	cout  << endl << domain.c_str() << endl << uri.c_str() << endl;
 }
 int main() {
 
-	Test1();
+	//Test1();
+	//TestString_2();
+	//TestInsert();
+	//TestErase();
+	TestFind();
 	return 0;
 }
--- Cpp_15_STL_string/Cpp_15_STL_string/string_me.h
+++ Cpp_15_STL_string/Cpp_15_STL_string/string_me.h
@@ -2,6 +2,7 @@
 
 #include <iostream>
 #include <assert.h>
+#include <cstring>
 using namespace std;
 
 namespace m_string {
@@ -28,11 +29,7 @@ namespace m_string {
 		const_iterator end() const {
 			return _str + _size;
 		}
-	private:
-		size_t _size;
-		size_t _capacity;
-		char* _str;
-	public:
+
 		//构造函数
 		//string(const char* str)	//初始化列表是按照成员变量在类中声明的次序进行初始化的
 		//	:_size(strlen(str))
@@ -52,7 +49,7 @@ namespace m_string {
 		string(const char* str = "") {
 			_size = strlen(str);
 			_capacity = _size;	//capacity表示可以存放的下的字符个数
-			_str = new char[_capacity + 1];		//开空间
+			_str = new char[_capacity + 1];		//开空间+1  要存放'\0'
 			strcpy(_str, str);	//拷贝数据
 		}
 		//将无参构造通过缺省参数合并
@@ -63,6 +60,14 @@ namespace m_string {
 		//	_str[0] = '\0';
 		//}
 
+		//拷贝构造  简单实现深拷贝
+		string(const string& str) {
+			_str = new char[str._capacity + 1];
+			strcpy(_str, str._str);
+			_size = str._size;
+			_capacity = str._capacity;
+		}
+		
 		//析构函数
 		~string() {
 			delete[] _str;
@@ -90,20 +95,21 @@ namespace m_string {
 		}
 
 		//可以用reserve来实现扩容
-		void reserve(size_t spaceSize) {	//spaceSize指的是字符的个数
-			if (spaceSize > _capacity) {
-				char* newSpace = new char[spaceSize + 1];	//多开一个空间存放\0
-				strcpy(newSpace, _str);
+		//考虑特殊情况的话，memcpy会更好
+		void reserve(size_t request_size) {	//request_size指的是要存放的字符的个数
+			if (request_size > _capacity) {	//请求的空间大于_capacity时，才扩容
+				char* newSpace = new char[request_size + 1];	//多开一个空间存放\0
+				strcpy(newSpace, _str);		//new是异地扩容
 				delete[] _str;
 				_str = newSpace;
-				_capacity = spaceSize;
+				_capacity = request_size;
 			}
 		}
 		//+=的本质是push_back因此先实现push_back
 		void push_back(char ch) {
 			//先考虑扩容
 			if (_size == _capacity) {
-				//二倍扩容
+				//二倍扩容,并防止空构造的字符串
 				reserve(_capacity == 0 ? 4 : _capacity * 2);
 			}
 			_str[_size++] = ch;	//放字符
@@ -116,7 +122,117 @@ namespace m_string {
 				reserve(_size + len);
 			}
 			strcpy(_str + _size, str);
-			_size += len;
+			_size += len;	//插入后_size要改变
 		}
+		string& operator+=(char ch) {
+			push_back(ch);
+			return *this;
+		}
+		string& operator+=(const char* str) {
+			append(str);
+			return *this;
+		}
+
+		void insert(size_t pos, size_t n, char ch) {
+			assert(pos <= _size);	//_size位置是字符串的末尾，可以在字符串的末尾插入
+			//扩容
+			if (_size + n > _capacity)
+				reserve(_size + n);
+			//挪动数据
+			//当传入的pos为0时，end会变成-1，end是size_t,-1会变成整形的最大值，会一直进入循环
+			//size_t end = _size;	
+			//int end = _size;	
+			//while (end >= (int)pos) {	//运算符两端 两个操作数类型不一致时，会进行提升
+			//						// 一般是范围小的向范围大的进行提升
+			//	_str[end + n] = _str[end];
+			//	--end;
+			//}
+			size_t end = _size;
+			while (end >= pos && end != npos) {
+				_str[end + n] = _str[end];
+				--end;
+			}
+			for (size_t i = 0; i < n; ++i) {
+				_str[pos + i] = ch;
+			}
+			_size += n;
+			_str[_size] = '\0';		//添加字符串结束标志
+		}
+		void insert(size_t pos, const char* str) {
+			assert(pos <= _size);	//_size位置是字符串的末尾，可以在字符串的末尾插入
+			size_t len = strlen(str);
+			//扩容
+			if (_size + len > _capacity)
+				reserve(_size + len);
+
+			size_t end = _size;
+			while (end >= pos && end != npos) {
+				_str[end + len] = _str[end];
+				--end;
+			}
+			for (size_t i = 0; i < len; ++i)
+				_str[pos + i] = str[i];
+
+			_size += len;
+			_str[_size] = '\0';		//添加字符串结束标志
+		}
+		//从pos位置开始删，删除len个字符，不传参默认删完
+		void erase(size_t pos, size_t len = npos) {
+			assert(pos);
+			if (len == npos || pos + len > _size) {		//这两种都是删完的情况
+				_str[pos] = '\0';
+				_size = pos;
+				_str[_size] = '\0';
+			}
+			else {
+				size_t end = pos + len;
+				while (end < _size) {	//end = '\0'时，可以把'\0'也挪过来
+					_str[pos++] = _str[end++];
+					//++end;
+				}
+				_size -= len;
+				_str[_size] = '\0';
+			}
+		}
+		//查找字符
+		size_t find(char ch, size_t pos = 0) const {
+			assert(pos < _size);
+			for (size_t i = pos; i < _size; ++i) {
+				if (_str[i] == ch) {
+					return i;
+				}
+			}
+			return npos;
+		}
+		size_t find(const char* str, size_t pos = 0) const {
+			assert(pos < _size);
+			const char* ptr = strstr(_str + pos, str);	//可以从不同的位置开始查找
+			if (ptr)
+				return ptr - _str;
+			else
+				return npos;
+		}
+
+		string substr(size_t pos = 0, size_t len = npos) const {
+			assert(pos < _size);
+			size_t n = len;
+			if (len == npos || (pos + len) > _size)
+				n = _size - pos;
+			string tmp;
+			tmp.reserve(n);
+			for (size_t i = pos; i < pos + n; ++i) {
+				tmp += _str[i];
+			}
+			return tmp;	//出了作用域，对象销毁，要用值传递
+		}
+	private:
+		size_t _size;
+		size_t _capacity;	//_capacity一般不包含\0
+		char* _str;
+		//static::size_t npos = -1;	//静态成员变量不能给缺省值，缺省值是给初始化列表用的
+		//可以这么用，但是不建议
+	public:
+		const static size_t npos;	//建议静态成员变量，声明和定义分离
 	};
+	const size_t string::npos = -1;
 }
