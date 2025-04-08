64f0e64027b75abf3dc5af9253795edc4666ae58
changan-memories <qinyuxiangzzu@outlook.com>
2025-04-08 21:08:07 +0800

    update vector

--- Cpp_16_STL_vector/Cpp_16_STL_vector/2_relize.cpp
+++ Cpp_16_STL_vector/Cpp_16_STL_vector/2_relize.cpp
@@ -1,6 +1,11 @@
 #include "vector.h"
-
-int main() {
+void print(const m_vector::vector<int>& v) {
+	for (auto& e : v) {
+		cout << e << " ";
+	}
+	cout << endl;
+}
+void testVector1() {
 	m_vector::vector<int> v1;
 	v1.push_back(1);
 	v1.push_back(2);
@@ -8,11 +13,49 @@ int main() {
 	v1.push_back(4);
 	v1.push_back(5);
 	v1.push_back(6);
+	v1.push_back(7);
+	v1.push_back(8);
+	v1.push_back(22);
+	v1.push_back(33);
 	for (auto& e : v1) {
 		cout << e << " ";
 	}
-
+	cout << endl;
+	for (auto& e : v1) {
+		e++;
+	}
+	print(v1);
+}
+void testVector2() {
+	m_vector::vector<int> v1;
+	v1.push_back(1);
+	v1.push_back(2);
+	v1.push_back(3);
+	v1.push_back(4);
+	v1.push_back(5);
+	v1.push_back(6);
+	v1.push_back(7);
+	v1.push_back(8);	//已有8个元素，再插入时会触发扩容逻辑
+	for (auto& e : v1) {
+		cout << e << " ";
+	}
+	cout << endl;
 	
+	m_vector::vector<int>::iterator p = v1.begin() + 3;
+	m_vector::vector<int>::iterator pres = v1.insert(p, 200);
+	*p + 200;
+	cout << *p << endl;
+	//p记录的是原空间地址，如果原空间发生了扩容，那么p就成了野指针,这里是野指针造成的迭代器失效
+	
+	//insert之后，迭代器可能会失效(扩容时迭代器失效)
+	//以后insert尽量不使用形参迭代器，因为insert后，迭代器可能失效
+	//正确的做法是，使用insert返回的结果当来获取pos的值
+	cout << *pres << endl;
+	print(v1);
+}
+int main() {
+	//testVector1();
+	testVector2();
 
 	return 0;
 }
--- Cpp_16_STL_vector/Cpp_16_STL_vector/vector.h
+++ Cpp_16_STL_vector/Cpp_16_STL_vector/vector.h
@@ -2,65 +2,120 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <assert.h>
 using namespace std;
 
 
 namespace m_vector {
 	template<class T>
 	class vector {
+	public:
 		typedef T* iterator;
 		typedef const T* const_iterator;
 	public:
+		//迭代器与const迭代器
 		iterator begin() { return _start; }
 		iterator end() { return _finish; }
-		const_iterator cbegin() const { return _start; }
-		const_iterator cend() const { return _finish; }
+		const_iterator begin() const { return _start; }
+		const_iterator end() const { return _finish; }
 
 		//默认构造函数
+		//我们写的逻辑是 构造时暂不开空间
 		vector()
 			:_start(nullptr)
 			, _finish(nullptr)
 			, _end_of_storage(nullptr)
 		{ }
 
+		~vector() {
+			if (_start) {
+				delete[] _start;
+				_finish = _start = _end_of_storage = nullptr;
+			}
+		}
 		void reserve(size_t n) {
 			// reserve 不是只给push_back使用，因此也要检查
 			if (n > capacity()) {
 				size_t old_sz = size();
-				T* tmp = new T[n];
+				T* tmp = new T[n];		//开空间,会构造一个vector
 				//原空间不为空时拷贝数据
 				if (_start) {
 					memcpy(tmp, _start, sizeof(T) * old_sz);
 					delete[] _start;
 				}
+				// start指向新空间，指针finish该为 start 加上 
 				_start = tmp;
-				_finish = _start + old_sz;
+				//_finish = _start + size();	//这是错误写法，size = finsih - start
+				_finish = _start + old_sz;	//让finsih指向和原空间一样的size处
 				_end_of_storage = _start + n;
 			}
 		}
+		//T可能是任意类型，自定义类型或内置类型，尽量使用引用
 		void push_back(const T& obj) {
-			//检查容量并扩容
-			if (_finish == _end_of_storage) {
-				//计算新空间需要的大小  二倍扩容逻辑
-				size_t newSpace = (capacity() == 0 ? 4 : capacity() * 2);	
-				reserve(newSpace);											//分配空间的大小
-			}
-			*_finish = obj;
-			++_finish;
+			////检查容量并扩容
+			//if (_finish == _end_of_storage) {
+			//	//计算新空间需要的大小  二倍扩容逻辑
+			//	size_t newCapacity = (capacity() == 0 ? 4 : capacity() * 2);	
+			//	// 传入二倍扩容后空间的大小
+			//	reserve(newCapacity);											//分配空间的大小
+			//}
+			//// 开好空间后，插入数据，并更新_finish
+			//*_finish = obj;
+			//++_finish;
+			
+			//insert(_finish, obj);
+			insert(end(), obj);
 		}
 
-		// 指针 - 指针，返回的是两个指针中间的元素个数
-		size_t capacity() const {
-			return _end_of_storage - _start;
+		iterator insert(iterator pos, const T& ele) {
+			//检查pos
+			assert(pos >= _start && pos <= _finish);
+			//检查容量并扩容
+
+			//扩容后，由于空间更新，pos记录的地址可能不是新开辟空间的地址，此时迭代器失效
+			if (_finish == _end_of_storage) {
+				//记录下 pos 相对于 _start的偏移地址
+				size_t len = pos - _start;
+
+				size_t newCapacity = capacity() == 0 ? 4 : capacity() * 2;
+				reserve(newCapacity);
+
+				//解决迭代器失效问题, 记录下便宜地址，解决的是函数内部迭代器失效的问题
+				pos = _start + len;
+			}
+			//插入逻辑
+			iterator end = _finish - 1;
+			while (end >= pos) {
+				*(end + 1) = *end;
+				--end;
+			}
+			//更新成员变量逻辑
+			*pos = ele;
+			++_finish;
+			return pos;		//返回插入位置的引用，解决外部迭代器失效问题
 		}
-		size_t size() const {
-			return _finish - _start;
+
+		void erase(iterator pos) {
+			assert(pos >= _start && pos <= _finish);
+
+		}
+		// 指针 - 指针，返回的是两个指针中间的元素个数
+		size_t capacity() const { return _end_of_storage - _start; }
+		size_t size() const { return _finish - _start; }
+
+		T& operator[](size_t pos) {
+			assert(pos < size());
+			return _start[pos];
+		}
+		const T& operator[](size_t pos) const{
+			assert(pos < size());
+			return _start[pos];
 		}
 	private:
-		//与STL中的命名风格保持一致
+		//与标准库STL中的命名风格保持一致
 		iterator _start;
 		iterator _finish;
 		iterator _end_of_storage;
 	};
-}
 
+}
