47352384d50ddce8cefbdcb0e1a2ca6facc3ec81
changan-memories <qinyuxiangzzu@outlook.com>
2025-04-14 11:24:46 +0800

    update test

---
+++ Test_demo/Test_demo/test.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	string s1("hello");
+	cout << s1 << endl << endl;
+	cin >> s1;
+	cout << endl << s1 << endl;
+	return 0;
+}
