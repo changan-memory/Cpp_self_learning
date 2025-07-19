54e208257bf2c847b2bd3668ebea6d6693651b31
changan-memory <qinyuxiangzzu@outlook.com>
2025-07-19 20:00:41 +0800

    review vector relize

---
+++ Cpp_16_STL_vector/Cpp_16_STL_vector/vector_review.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <assert.h>
+#include <string.h>
+#include <stdlib.h>
+using namespace std;
+
+// ÆÚÄ©»Ø¹é¸´Ï°°æ±¾
+namespace vector_m {
+	class vector {
+
+	};
+}
