cb62e2d135f1636a3d06df9c5a832fb708b46dab
changan-memory <qinyuxiangzzu@outlook.com>
2025-09-26 21:54:29 +0800

    LeetCode - List

--- Test_demo/debug.cpp
+++ Test_demo/debug.cpp
@@ -4,43 +4,58 @@
 
 using namespace std;
 
-class Rate {
-public:
-	Rate(double rate) : _rate(rate)
-	{
-	}
-	double operator()(double money, int year)
-	{
-		return money * _rate * year;
-	}
-private:
-	double _rate;
+
+struct ListNode {
+    int val;
+    struct ListNode *next;
+    ListNode(int x = 0) : val(x), next(NULL) {}
 };
+ListNode* creatLiist() {
+    ListNode* node1 = new ListNode(1);
+    ListNode* node2 = new ListNode(5);
+    ListNode* node3 = new ListNode(2);
+    ListNode* node4 = new ListNode(7);
+    ListNode* node5 = new ListNode(3);
+    ListNode* node6 = new ListNode(4);
+    node1->next = node2;
+    node2->next = node3;
+    node3->next = node4;
+    node4->next = node5;
+    node5->next = node6;
+    return node1;
+}
+class Partition {
+public:
+    ListNode* partition(ListNode* pHead, int x) {
+        if (pHead == nullptr)
+            return nullptr;
+        ListNode* guardLess = new ListNode(0);
+        ListNode* guardGreater = new ListNode(0);
+        guardLess->next = guardGreater->next = nullptr;
 
-//int main() {
-//	int a = 1, b = 2, c = 3, d = 4;
-//	const int e = 6;
-//
-//	cout << a << " " << b << " " << c << " " << d << " " << e << endl;
-//	// 引用捕获副 父作用域所有变量，值捕获 c 变量
-//	cout << &e << endl;
-//	auto func1 = [&, c]() {
-//		++a;
-//		++b;
-//		//++c;	// 值捕获的 c 不可修改
-//		++d;
-//		cout << &e << endl;
-//		cout << typeid(e).name() << endl;
-//		};
-//	func1();
-//	cout << a << " " << b << " " << c << " " << d << " " << e << endl;
-//	return 0;
-//}
-
+        ListNode* lessTail = guardLess, * greaterTail = guardGreater;
+        ListNode* curNode = pHead;
+        while (curNode) {
+            if (curNode->val < x) {
+                lessTail->next = curNode;
+                lessTail = lessTail->next;
+            }
+            else {
+                greaterTail->next = curNode;
+                greaterTail = greaterTail->next;
+            }
+            curNode = curNode->next;
+        }
+        // 链接两个链表
+        lessTail->next = guardGreater->next;
+        pHead = guardLess->next;
+        delete guardGreater;
+        delete guardLess;
+        return pHead;
+    }
+};
 int main() {
-	int x = 0, y = 1, z = 2;
-	printf("%d\n", x);
-	printf("%d %d\n", x, y);
-	printf("%d %d %d\n", x, y, z);
-	return 0;
+    Partition p1;
+    p1.partition(creatLiist(), 5);
+    return 0;
 }
